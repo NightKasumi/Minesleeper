9880ad1a5b93291544d1b5ae2dd98d46574ea71d
Haru Nyan <pysellj@gmail.com>
2024-05-15 18:55:41 +0000

    Removed bug that lets you see all correc

--- SleeperWindow.java
+++ SleeperWindow.java
@@ -98,8 +98,6 @@ public class SleeperWindow extends Main {
                                     if (mineList.contains(tile.r + "," + tile.c)) {
                                         correctFlags++;
                                     }
-                                    System.out.println("correctFlags = " + correctFlags);
-                                    System.out.println("mines = " + mines);
                                     if (correctFlags == mines) {
                                         textLabel.setText("You win, but you get nothing!!!");
                                         gameOver = true;
